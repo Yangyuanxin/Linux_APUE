90561b75f4ee452a1dfbfaee3d7d9a1be899d1d6
Yangyuanxin <morixinguan@vip.qq.com>
2022-08-15 11:41:52 -0400

    Add SystemInfo

---
+++ LinuxSystemInfo/SystemInfo.c
@@ -0,0 +1,21 @@
+/****************************************************************************
+  Copyright(C)    2022, morixinguan@vip.qq.com
+  File name :     SystemInfo.c
+  Author :        Yangyuanxin
+  Version:        1.0
+  Description:    系统信息相关接口及测试程序实现
+  Other:
+  Mode History:
+          <author>        <time>      <version>   <desc>
+          Yangyuanxin    2022-8-15     V1.0.0.0
+****************************************************************************/
+#include "SystemInfo.h"
+
+
+
+#ifdef TEST_OPEN
+S32 SystemInfoTest(Void)
+{
+
+}
+#endif
---
+++ LinuxSystemInfo/SystemInfo.h
@@ -0,0 +1,29 @@
+/****************************************************************************
+  Copyright(C)    2022, morixinguan@vip.qq.com
+  File name :     SystemInfo.h
+  Author :        Yangyuanxin
+  Version:        1.0
+  Description:    系统信息相关接口及测试程序实现
+  Other:
+  Mode History:
+          <author>        <time>      <version>   <desc>
+          Yangyuanxin    2022-8-15     V1.0.0.0
+****************************************************************************/
+#ifndef _SYSTEMINFO_H_
+#define _SYSTEMINFO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "SystemInfo.h"
+
+
+#ifdef TEST_OPEN
+S32 SystemInfoTest(Void);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif  //ifndef _SYSTEMINFO_H_
+/****************************************************************************/
